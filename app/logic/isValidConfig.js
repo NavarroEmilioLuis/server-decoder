f16ab506ffd12c2f850a17b7e1eb474bc9bc27a6
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-07-22 13:47:29 -0500

    Update and rename config validation function

--- app/logic/isValidConfig.js
+++ app/logic/isValidConfig.js
@@ -1,15 +1,22 @@
 /*
-  Validates all 3 params used to create a game
-  are valid.
+  Validates if the config used to create a game
+  is valid.
 
   attempts: int (1 to Infinity)
   size: int (1 to 16)
   colors: array<string>
+  duplicates: boolean
+  blanks: boolean
 
   Return value: boolean
 */
-export function isValidGame(attempts, size, colors) {
+export function isValidConfig(config) {
+  const { attempts, size, colors, duplicates, blanks } = config;
   // Early bail if conditions aren't met
+  // Ensure duplicates and blanks are booleans
+  if (typeof duplicates !== 'boolean' || typeof blanks !== 'boolean')
+    return false;
+
   // Ensure attempts is a number from 1 to Infinity
   if (typeof attempts !== 'number' || attempts < 1) return false;
 
