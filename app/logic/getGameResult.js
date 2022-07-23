61035efa4c337f960c6d1e8081f7359db8d91d82
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-07-23 16:55:55 -0500

    Update logic that determines the result of a game

--- app/logic/getGameResult.js
+++ app/logic/getGameResult.js
@@ -1,14 +1,14 @@
 /*
   Takes an existing game and returns the result by checking
-  the state of the game with the new code attempt.
+  the state of the game and how many position matches the new code has.
 
   game: object {}
-  code: array<string>
+  positionMatches: number
 
   Return value: number (0, 1) || null
 */
-export function getGameResult(game, code) {
-  if (game.state.code === code) return 1;
+export function getGameResult(game, positionMatches) {
+  if (game.config.size === positionMatches) return 1;
   else if (game.state.currentAttempt === game.config.attempts) return 0;
   return null;
 }
