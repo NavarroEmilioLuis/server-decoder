55fad997b1db0335221975b5daafa729c142673f
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-07-23 16:51:58 -0500

    Actually use proper function name

--- app/logic/getGameResult.js
+++ app/logic/getGameResult.js
@@ -7,7 +7,7 @@
 
   Return value: number (0, 1) || null
 */
-export function updateGame(game, code) {
+export function getGameResult(game, code) {
   if (game.state.code === code) return 1;
   else if (game.state.currentAttempt === game.config.attempts) return 0;
   return null;
