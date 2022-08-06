43cc6d7b66b2839ee2844ce12a1d4fe0b97bb98c
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-06 13:49:08 -0500

    Use a lower salt round default to avoid waiting forever

--- app/auth/createPasswordHash.js
+++ app/auth/createPasswordHash.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
-const SALT_ROUNDS = process.env.SALT_ROUNDS || 1337;
+const SALT_ROUNDS = process.env.SALT_ROUNDS || 13.37;
 export async function createPasswordHash(password) {
   return bcrypt.hash(password, SALT_ROUNDS);
 }
