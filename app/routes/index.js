9e188f1ab5e0d723d38e157009b65cf2a401d432
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-06 14:11:19 -0500

    Include login and register routes, separate public routes from private ones

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import { about } from './about.js';
 import { leaderboard } from './leaderboard.js';
+import { login } from './login.js';
 import { play } from './play.js';
+import { register } from './register.js';
 import { stats } from './stats.js';
+
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const routes = express.Router();
 
+// Public routes
 routes.use('/about', about);
 routes.use('/leaderboard', leaderboard);
+routes.use('/login', login);
+routes.use('/register', register);
+
+// Private routes
+routes.use(authMiddleware);
 routes.use('/play', play);
 routes.use('/stats', stats);
 
