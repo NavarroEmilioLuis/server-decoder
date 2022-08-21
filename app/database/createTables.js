151a615ce95d7398437b9c5ee03e71939954fdb0
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-21 10:49:22 -0500

    Track user register date, create stats table

--- app/database/createTables.js
+++ app/database/createTables.js
@@ -6,8 +6,21 @@ export async function createTables(db) {
     `CREATE TABLE IF NOT EXISTS $1:name (
       id SERIAL PRIMARY KEY,
       username VARCHAR(32) NOT NULL UNIQUE,
-      password TEXT NOT NULL
+      password TEXT NOT NULL,
+      joined_at DATE NOT NULL DEFAULT CURRENT_DATE
     );`,
     ['users']
+  );
+
+  await db.none(
+    `CREATE TABLE IF NOT EXISTS $1:name (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES $2:name($3:name),
+      games_played INTEGER,
+      max_streak INTEGER,
+      current_streak INTEGER,
+      score INTEGER
+    );`,
+    ['stats', 'users', 'id']
   );
 }
